582a6a880cb62fdfca7b0f2cf1e18fd334014418
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-01-13 15:26:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![logo](scope_logo.svg)
+
+# Scope for Python
+
+GitHub Action to run your tests automatically instrumented with the [Scope Python agent](http://home.undefinedlabs.com/goto/python-agent).
+
+## About Scope
+
+[Scope](https://scope.dev) gives developers production-level visibility on every test for every app – spanning mobile, monoliths, and microservices.
+
+## Usage
+
+1. Set Scope DSN inside Settings > Secrets as `SCOPE_DSN`.
+2. Add a step to your GitHub Actions workflow YAML that uses this action:
+
+```yml
+steps:
+  - uses: actions/checkout@v1
+  - uses: actions/setup-python@v1
+    with:
+      python-version: "3.x"
+  - name: Install dependencies
+    run: pip install -r requirements.txt
+  - name: Scope for Python
+    uses: undefinedlabs/scope-for-python-action@v1
+    with:
+      dsn: ${{secrets.SCOPE_DSN}} # required
+      command: pytest # optional - default is 'pytest'
+```
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+
+const SCOPE_DSN = 'SCOPE_DSN'
+
+const DEFAULT_COMMAND = 'pytest'
+
+async function run() {
+  try {
+    const command = core.getInput('command') || DEFAULT_COMMAND
+    const dsn = core.getInput('dsn') || process.env[SCOPE_DSN]
+
+    if (!dsn) {
+      throw Error('Cannot find the Scope DSN')
+    }
+
+    let apiEndpoint, apiKey
+    try {
+      const { username, origin } = new URL(dsn)
+      apiEndpoint = origin
+      apiKey = username
+    } catch (e) {}
+
+    if (!apiEndpoint || !apiKey) {
+      throw Error('SCOPE_DSN does not have the correct format')
+    }
+
+    console.log(`Command: ${command}`)
+    if (dsn) {
+      console.log(`DSN has been set.`)
+    }
+
+    await exec.exec('pip install scopeagent', null, {
+      ignoreReturnCode: true,
+    })
+
+    return ExecScopeRun(`scope-run ${command}`, apiEndpoint, apiKey)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+function ExecScopeRun(command, apiEndpoint, apiKey) {
+  return exec.exec(command, null, {
+    env: {
+      ...process.env,
+      SCOPE_API_ENDPOINT: apiEndpoint,
+      SCOPE_APIKEY: apiKey,
+      SCOPE_AUTO_INSTRUMENT: true,
+    },
+  })
+}
+
+run()
